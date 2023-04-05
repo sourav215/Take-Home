965a7861b4c1e65731392313e503a1f44e8d48ff
sourav215 <sourav10010080@gmail.com>
2023-04-05 16:04:14 +0530

    Navbar and Table Component added

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,74 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  useDisclosure,
+  Stack,
+  Divider,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+
+const Links = [
+  { title: "Home", path: "/" },
+  { title: "Travel", path: "/travel" },
+];
+
+const NavLink = ({ title, path }) => <Link to={path}>{title}</Link>;
 
 const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/travel">Travel</Link>
-        </li>
-        <li></li>
-      </ul>
-    </div>
+    <>
+      <Box bg="white.100" px={4}>
+        <Flex
+          h={16}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          fontSize={"xl"}
+        >
+          <HStack spacing={8} alignItems={"center"}>
+            <Box fontSize={"2xl"} fontWeight={"semibold"}>
+              {" "}
+              <Link to={"/"}>Travelopia</Link>
+            </Box>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <HStack
+              as={"nav"}
+              spacing={20}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map(({ title, path }) => (
+                <NavLink key={title} title={title} path={path} />
+              ))}
+            </HStack>
+          </Flex>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map(({ title, path }) => (
+                <NavLink key={title} title={title} path={path} />
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+      <Divider />
+    </>
   );
 };
 
--- client/src/Pages/Travel/Travel.jsx
+++ client/src/Pages/Travel/Travel.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react";
 
 const Travel = () => {
   return (
-    <div>Travel</div>
-  )
-}
+    <TableContainer m={"10"}>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Email Address</Th>
+            <Th>Destination</Th>
+            <Th isNumeric>No of Travellers</Th>
+            <Th isNumeric>Budget Per Person (USD)</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>Sourav Jana</Td>
+            <Td>sourav10080@gmail.com</Td>
+            <Td>India</Td>
+            <Td isNumeric>3</Td>
+            <Td isNumeric>$ 1200</Td>
+          </Tr>
+          <Tr>
+            <Td>Sourav Jana</Td>
+            <Td>sourav10080@gmail.com</Td>
+            <Td>India</Td>
+            <Td isNumeric>3</Td>
+            <Td isNumeric>$ 1200</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
 
-export default Travel
+export default Travel;
